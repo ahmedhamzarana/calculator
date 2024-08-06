59e23808edd6bdfda122e4ba5d4150b273c2d138
Hamza Rana <ahmedhamzarana2244@gmail.com>
2024-08-06 20:46:53 +0500

    i will make a calculator ny using js, css and html

---
+++ script.js
@@ -0,0 +1,23 @@
+function appendCharacter(character) {
+    const display = document.getElementById('display');
+    display.value += character;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
